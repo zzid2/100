724bdfd24ca6df754669f9864d871a1a8ffd5ac7
qq <qq@qq.com>
2024-07-27 22:14:09 +0800

    微信通知
    Signed-off-by: qq <qq@qq.com>

--- build/DIY/diy-part2.sh
+++ build/DIY/diy-part2.sh
@@ -23,6 +23,9 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 # 修改 root密码登录为空（免密码登录）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
+# ttyd 自动登录
+sed -i "s?/bin/login?/usr/libexec/login.sh?g" package/feeds/packages/ttyd/files/ttyd.config
+
 # 修改主机名（不能纯数字或者中文）
 # sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate                                                                    ## 把初始默认的“OpenWrt” 修改为：G-DOCK
 # sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrtx86'' package/lean/default-settings/files/zzz-default-settings      ## 再初始默认基础上修改为：OpenWrtx86
@@ -42,9 +45,15 @@ sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].zonename='Asia\/Shanghai'/g"
 # 修改输出固件名称（参考：Lede-20240726-openwrt）
 # sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=Lede-$(shell date +%Y-%m-%d)-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
+# 添加自定义软件包
+# echo '
+# CONFIG_PACKAGE_luci-app-mosdns=y
+# CONFIG_PACKAGE_luci-app-adguardhome=y
+# CONFIG_PACKAGE_luci-app-openclash=y
+# ' >> .config
 
-
-
+# 修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
 
 
@@ -89,5 +98,21 @@ sed -i 's/"KMS 服务器"/"KMS激活"/g' feeds/luci/applications/luci-app-vlmcsd
 
 
 
+# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+# sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
+# sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
+# sed -i 's/"aMule设置"/"电驴下载"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-vsftpd/po/zh-cn/vsftpd.po
+# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
+# sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
+# sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
+# sed -i 's/"KMS 服务器"/"KMS激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
+# sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
+# sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
+# sed -i 's/TTYD 终端/命令窗/g' feeds/luci/transplant/luci-app-ttyd/po/zh-cn/terminal.po
+# sed -i 's/"Web 管理"/"Web管理"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
+# sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+# sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 
 
--- build/lede_N1/diy_oem.sh
+++ build/lede_N1/diy_oem.sh
@@ -2,14 +2,29 @@
 lede_path=$(pwd)                         ## 赋于成变量= 当前执行目录，lede源码目录；
 cd $lede_path                            ## 进入（Lede目录）内并执行操作；
 
+# 字体颜色配置
+print_error() {                           ## 打印红色字体
+    echo -e "\033[31m$1\033[0m"
+}
+
+print_green() {                           ## 打印绿色字体
+    echo -e "\033[32m$1\033[0m"
+}
+
+print_yellow() {                          ## 打印黄色字体
+    echo -e "\033[33m$1\033[0m"
+}
+
+
+print_yellow "正在执行diy-oem.sh脚本......"
 # 修改主机名（不能纯数字或者中文）
 file="package/lean/default-settings/files/zzz-default-settings"      ## 文件的路径
 hostname_line="uci set system.@system[0].hostname='N1'"              ## 插入的内容（默认OpenWrtx86=替换为主机名）
 if grep -q "uci set system.@system\[0\].hostname=" "$file"; then
-    echo "注意：已存在主机名，替换为新名称"
+    print_green "注意：已存在主机名，替换为新名称"
     sed -i "s|uci set system.@system\[0\].hostname=.*|$hostname_line|" "$file"
 else
-    echo "注意：不存在主机名，插入新名称"
+    print_green "注意：不存在主机名，插入新名称"
     sed -i "/uci commit system/i\\$hostname_line" "$file"
 fi
 # 修改wifi名称
